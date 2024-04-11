5f2b6186954a7d91123f47dcd1a02ae1449691c3
blremilie <bletteryemilie@gmail.com>
2024-04-11 10:15:53 +0200

    mise à jour js

--- script.js
+++ script.js
@@ -1,25 +1,47 @@
-function isLeapYear(year) {
-  year = parseInt(document.getElementById("inputYear").value);
-  if (year % 4 !== 0) {
-    return false;
-  }
-  if (year % 100 === 0 && year % 400 !== 0) {
-    return false;
-  }
-  return true;
+function formatDate (inputDay, inputMonth, inputYear){
+
+  let dateInput = inputDay + "/" + inputMonth + "/" + inputYear;
+  return dateInput
 }
 
-function validateInput(inputDay, inputMonth, inputYear) {
-  if(inputDay == "" || inputMonth == "" || inputYear == "") {
-      alert("This field is required")
-      return false
+function validateDate(date) {
+  const [day,month,year] = date.split("/"); 
+  let errorTable = [];
+
+  if (!day) {
+    errorTable.append (document.getElementById ("required_day"))
   }
-  else if(!isLeapYear(inputYear) && parseInt(inputMonth) === 2 && parseInt(inputDay) >= 29) {
-      alert("Day is to high")
-      return false
+  if (!month) {
+    errorTable.append (document.getElementById ("required_month"))
   }
-  else {
-      return true
+  if (!year) {
+    errorTable.append (document.getElementById ("required_year"))
+  }
+
+  const dayNum = parseInt(day, 10);
+  const monthNum = parseInt(month, 10);
+  const yearNum = parseInt(year, 10);
+
+  if (isNaN(yearNum) || yearNum.lenght !== 4) {
+    errorTable.append (document.getElementById("valid_year"))
+  }
+
+  if (isNaN(monthNum) || monthNum < 1 || monthNum > 12) {
+    errorTable.append (document.getElementById("valid_month"))
+  }
+
+  const dayInMonth = new Date(yearNum, monthNum, 0).getDate();
+
+  if (isNaN(dayNum) || dayNum < 1 || dayNum > dayInMonth) {
+    errorTable.append (document.getElementById("valid_day"))
+  }
+
+  return errorTable
+}
+
+function applyErrorStyle (errorTable){
+  for (let index=0; index <errorTable.lenght; index ++) {
+    errorTable[index].stye.display= block
   }
 }
 
@@ -37,6 +59,15 @@ function calculateDate (inputDay, inputMonth, inputYear){
 
   return [differenceInYears, differenceInMonths, remainderDaysAfterMonths]
 }
+
+let ageTable = calculateAge(inputDay, inputMonth,inputYear);
+
+function applyCalculateStyle (ageTable){
+   document.getElementById ("result_years").innerHTML = ageTable[0]
+   document.getElementById("result_months").innerHTML = ageTable[0]
+   document.getElementById("result_days").innerHTML = ageTable[0]
+}
+
 let button = document.getElementById("button");
 
 button.addEventListener('click', function() {
